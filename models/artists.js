214b940951af006e24b3900fdc8e792ae7e7b734
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-04-05 11:42:10 -0700

    masterapi

--- models/artists.js
+++ models/artists.js
@@ -7,15 +7,21 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // create a Artists schema, define how our object will be structured
+// possible updated schema
+// 1. searchQuery = string of user
+// 2. likeBands x6
+// 3 album art-lastfm url image for each like band
+// userid or email - auth0
 
 const artistsSchema = new Schema({
+  search: {type: String, require: true},
   name: {type: String, require: true},
   genre: {type: String, require: true},
   match: {type: Number, require: true},
-  favorite: {type: Boolean, require: true},
-  search: {type: String, require: true},
-  img: {type: String}
+  img: {type: String},
+  favorite: {type: Boolean, require: true}
 });
+
 
 const ArtistModel = mongoose.model('Artists', artistsSchema);
 
